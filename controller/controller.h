099a3d71b79b08f26043f9e57e92e6a4f057113f
Thomas Rinsma <thomasrinsma@gmail.com>
2012-12-28 22:10:37 +0100

    Camera also working vertically now :)

--- controller/controller.h
+++ controller/controller.h
@@ -8,46 +8,61 @@ class Controller
 {
 	typedef sf::Key::Code KeyCode_t;
 
-	sf::Input const *d_input;
+	sf::Window *d_win;
 
 	// These contain the key mappings
 	KeyCode_t d_keyForward;
 	KeyCode_t d_keyBack;
 	KeyCode_t d_keyLeft;
 	KeyCode_t d_keyRight;
+	KeyCode_t d_keyUp;
+	KeyCode_t d_keyDown;
 
 	public:
-		Controller(sf::Input const *input, KeyCode_t keyForward = KeyCode_t::W, KeyCode_t keyBack = KeyCode_t::S, 
-			KeyCode_t keyLeft = KeyCode_t::A, KeyCode_t keyRight = KeyCode_t::D);
+		Controller(sf::Window *win, KeyCode_t keyForward = KeyCode_t::W, KeyCode_t keyBack = KeyCode_t::S, 
+			KeyCode_t keyLeft = KeyCode_t::A, KeyCode_t keyRight = KeyCode_t::D,
+			KeyCode_t keyUp = KeyCode_t::Space, KeyCode_t keyDown = KeyCode_t::LControl);
 
 		// Getters
 		bool moveForward() const;
 		bool moveBack() const;
 		bool moveLeft() const;
 		bool moveRight() const;
+		bool moveUp() const;
+		bool moveDown() const;
 
-		glm::vec2 mousePosition() const;
+		glm::vec2 mouseOffset();
 };
 
 
 inline bool Controller::moveForward() const
 {
-	return d_input->IsKeyDown(d_keyForward);
+	return d_win->GetInput().IsKeyDown(d_keyForward);
 }
 
 inline bool Controller::moveBack() const
 {
-	return d_input->IsKeyDown(d_keyBack);
+	return d_win->GetInput().IsKeyDown(d_keyBack);
 }
 
 inline bool Controller::moveLeft() const
 {
-	return d_input->IsKeyDown(d_keyLeft);
+	return d_win->GetInput().IsKeyDown(d_keyLeft);
 }
 
 inline bool Controller::moveRight() const
 {
-	return d_input->IsKeyDown(d_keyRight);
+	return d_win->GetInput().IsKeyDown(d_keyRight);
+}
+
+inline bool Controller::moveUp() const
+{
+	return d_win->GetInput().IsKeyDown(d_keyUp);
+}
+
+inline bool Controller::moveDown() const
+{
+	return d_win->GetInput().IsKeyDown(d_keyDown);
 }
         
 #endif
--- game/stepGame.cc
+++ game/stepGame.cc
@@ -5,26 +5,7 @@ void Game::stepGame()
 	if (not d_running)
 		return;
 
-	// TODO
-	float mouseSpeed = 0.001f * M_PI; // radians per pixel
-	float moveSpeed = 2.0f; // units per second
+	float deltaTime = d_win->GetFrameTime();
 
-	int latMovement = d_controller->moveRight() * 1.0f - d_controller->moveLeft() * 1.0f;
-	int longMovement = d_controller->moveForward() * 1.0f - d_controller->moveBack() * 1.0f;
-
-	float latSpeed = 1.0f;
-	float longSpeed = 1.0f;
-
-	// Mouse stuff
-	vec2 mousePosDelta = d_controller->mousePosition() - vec2(d_win->GetWidth() / 2, d_win->GetHeight() / 2);
-
-	//cout << "mouseDeltaX: " << mousePosDelta.x << ", mouseDeltaY: " << mousePosDelta.y << endl;
-	d_player.setYaw(d_player.yaw() - mousePosDelta.x * mouseSpeed);
-	d_player.setPitch(d_player.pitch() - mousePosDelta.y * mouseSpeed);
-
-	// Reset cursor to center
-	d_win->SetCursorPosition(d_win->GetWidth() / 2, d_win->GetHeight() / 2);
-
-	d_player.setRelativeVelocity(vec3(latMovement * latSpeed, 0.0f, longMovement * longSpeed)); // TODO: up/down
-	d_player.integratePosition(d_win->GetFrameTime() * moveSpeed);
+	d_player.update(deltaTime, d_controller);
 }
