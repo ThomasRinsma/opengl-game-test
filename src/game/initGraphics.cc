9f15c31bcd311e54c955c6bf92832b84f74c67f9
ThomasRinsma <ThomasRinsma@localhost>
2013-01-20 21:12:53 +0100

    implemented FOV changing with scrollwheel

--- src/game/initGraphics.cc
+++ src/game/initGraphics.cc
@@ -6,12 +6,7 @@ void Game::initGraphics()
 	d_shaderPrograms.push_back(new ShaderProgram("simpleTexture", {"m", "v", "p", "n"}));
 	d_shaderPrograms.push_back(new ShaderProgram("simpleText", {"mvp", "c"}));
 	
-	d_projMat = glm::perspective(
-		45.0f,	// 45 degree FOV
-		static_cast<float>(d_win.GetWidth()) /
-		static_cast<float>(d_win.GetHeight()),  // Ratio
-		0.1f, 100.0f // near/far
-	);
+	setProjMat(d_fov, 0.1f, 100.0f);
 
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
