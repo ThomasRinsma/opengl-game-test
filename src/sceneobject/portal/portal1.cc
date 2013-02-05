3b6928b93ee651c86266ead451c07e3ce88eb58d
Thomas Rinsma <thomasrinsma@gmail.com>
2013-02-05 19:45:42 +0100

    attribute fix in Portal

--- src/sceneobject/portal/portal1.cc
+++ src/sceneobject/portal/portal1.cc
@@ -14,21 +14,27 @@ Portal::Portal(ShaderProgram &shaderProgram, Entity *destination)
 
 	// Portal vertices:   X, Y, Z
 	float vertices[] = {
-		-0.5f, -0.5f,  0.0f,
-		 0.5f, -0.5f,  0.0f,
-		 0.5f,  0.5f,  0.0f,
-
-		 0.5f,  0.5f,  0.0f,
-		-0.5f,  0.5f,  0.0f,
-		-0.5f, -0.5f,  0.0f,
+		-0.5f, -0.5f,  0.0f,    0.0f, 0.0f, 0.0f,
+		 0.5f, -0.5f,  0.0f,    0.0f, 0.0f, 0.0f,
+		 0.5f,  0.5f,  0.0f,    0.0f, 0.0f, 0.0f,
+		 0.5f,  0.5f,  0.0f,    0.0f, 0.0f, 0.0f,
+		-0.5f,  0.5f,  0.0f,    0.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f,  0.0f,    0.0f, 0.0f, 0.0f,
 	};
 
 	// Copy vertex data into VBO
 	glBindBuffer(GL_ARRAY_BUFFER, d_vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	
+
 	// Link array buffer to "position" attribute
 	GLint posAttrib = d_shaderProgram.attribLocation("position");
 	glEnableVertexAttribArray(posAttrib);
-	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
+
+
+	// Link array buffer to "color" attribute
+	GLint colAttrib = d_shaderProgram.attribLocation("color");
+	glEnableVertexAttribArray(colAttrib);
+	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
 }
