0a6fbecd877bdadf9f187d3f74054aa657e60547
ThomasRinsma <thomasrinsma@gmail.com>
2013-04-16 17:15:07 +0200

    fixed some TODOs

--- src/gui/gui.h
+++ src/gui/gui.h
@@ -6,7 +6,6 @@
 #include <SFML/Window.hpp> // for sf::Window
 #include <glm/glm.hpp> // for glm::mat4
 #include "../drawableentity/drawableentity.h"
-#include "../player/player.h"
 
 class GUI
 {
@@ -14,12 +13,8 @@ class GUI
 	glm::mat4 d_viewMat;
 	glm::mat4 d_projMat;
 
-    // Store Player to retrieve info from?
-    // TODO: is this necesairy?
-    Player &d_player;
-
     public:
-        GUI(sf::Window &win, Player &player);
+        GUI(sf::Window &win);
         ~GUI();
 
         void add(std::string const &name, DrawableEntity *object);
@@ -27,14 +22,8 @@ class GUI
         void draw();
 
         DrawableEntity *get(std::string const &name);
-        //glm::mat4 &getProjMat() const;
 
     private:
 };
-
-//inline glm::mat4 &GUI::getProjMat() const
-//{
-//	return d_projMat;
-//}
         
 #endif
--- src/gui/gui1.cc
+++ src/gui/gui1.cc
@@ -1,8 +1,7 @@
 #include "gui.ih"
 
-GUI::GUI(sf::Window &win, Player &player)
+GUI::GUI(sf::Window &win)
 :
 	d_viewMat(glm::mat4(1.0f)),
-	d_projMat(glm::ortho(0.0f, (float)win.getSize().x, 0.0f, (float)win.getSize().y, -1.0f, 1.0f)),
-	d_player(player)
+	d_projMat(glm::ortho(0.0f, (float)win.getSize().x, 0.0f, (float)win.getSize().y, -1.0f, 1.0f))
 {}
