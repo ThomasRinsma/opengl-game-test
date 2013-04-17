3fe1baa3eefa4028b213aa9451b7a609c09c3486
Thomas Rinsma <thomasrinsma@gmail.com>
2013-04-17 16:31:02 +0200

    implemented recursive portals, finally

--- src/scene/draw.cc
+++ src/scene/draw.cc
@@ -1,69 +1,154 @@
 #include "scene.ih"
+#include <iostream>
 
-void Scene::draw(Player const &player)
+void Scene::drawRecursivePortals(glm::mat4 const &viewMat, glm::mat4 const &projMat, size_t recursionLevel)
 {
-	glEnable(GL_DEPTH_TEST);
-	
-	// Enable stencil test, increment on fail
-	glEnable(GL_STENCIL_TEST);
-	glStencilOp(GL_INCR, GL_KEEP, GL_KEEP);
-
 	for (Portal *portal : d_portals)
 	{
 		// Disable color and depth drawing
 		glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
 		glDepthMask(GL_FALSE);
 
-		// Always fail stencil test (on the portal pixels)
-		glStencilFunc(GL_NEVER, 0, 0xFF);
+		// Disable depth test
+		glDisable(GL_DEPTH_TEST);
 
-		// Enable all stencil bits
+		// Enable stencil test, to prevent drawing outside
+		// region of current portal depth
+		glEnable(GL_STENCIL_TEST);
+
+		// Fail stencil test when inside of outer portal
+		// (fail where we should be drawing the inner portal)
+		glStencilFunc(GL_NOTEQUAL, recursionLevel, 0xFF);
+
+		// Increment stencil value on stencil fail
+		// (on area of inner portal)
+		glStencilOp(GL_INCR, GL_KEEP, GL_KEEP);
+
+		// Enable (writing into) all stencil bits
 		glStencilMask(0xFF);
 
-		// Draw portal into empty stencil buffer
-		glClear(GL_STENCIL_BUFFER_BIT);
-		portal->draw(player.viewMat(), player.projMat());
+		// Draw portal into stencil buffer
+		portal->draw(viewMat, projMat);
 		
 		
-		// View matrix as if the player was already teleported
-		glm::mat4 destView = player.viewMat() * portal->modelMat()
-			* glm::rotate(glm::mat4(1.0f), 180.0f, glm::vec3(0.0f, 1.0f, 0.0f))
+		// Calculate view matrix as if the player was already teleported
+		glm::mat4 destView = viewMat * portal->modelMat()
+			* glm::rotate(glm::mat4(1.0f), 180.0f, glm::vec3(0.0f, 1.0f, 0.0f) * portal->orientation())
 			* glm::inverse(portal->destination()->modelMat());
 
+		// Base case, render inside of inner portal
+		if (recursionLevel == 2)
+		{
+			// Enable color and depth drawing
+			glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+			glDepthMask(GL_TRUE);
+			
+			// Clear the depth buffer so we don't interfere with stuff
+			// outside of this inner portal
+			glClear(GL_DEPTH_BUFFER_BIT);
 
-		// Enable color and depth drawing
-		glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
-		glDepthMask(GL_TRUE);
+			// Enable the depth test
+			// So the stuff we render here is rendered correctly
+			glEnable(GL_DEPTH_TEST);
 
-		// Disable drawing into stencil buffer
-		glStencilMask(0x00);
+			// Enable stencil test
+			// So we can limit drawing inside of the inner portal
+			glEnable(GL_STENCIL_TEST);
 
-		// Draw only where stencil value >= 1  (so 1 <= stencil value)
-		glStencilFunc(GL_LEQUAL, 1, 0xFF);
+			// Disable drawing into stencil buffer
+			glStencilMask(0x00);
 
-		// Draw scene objects with destView, limited to stencil buffer
-		// use an edited projection matrix to set the near plane to the portal plane
-		drawNonPortals(destView, portal->clippedProjMat(destView, player.projMat()));
+			// Draw only where stencil value == recursionLevel + 1
+			// which is where we just drew the new portal
+			glStencilFunc(GL_EQUAL, recursionLevel + 1, 0xFF);
+
+			// Draw scene objects with destView, limited to stencil buffer
+			// use an edited projection matrix to set the near plane to the portal plane
+			drawNonPortals(destView, portal->clippedProjMat(destView, projMat));
+			//drawNonPortals(destView, projMat);
+		}
+		else
+		{
+			// Recursion case
+			// Pass our new view matrix and the clipped projection matrix (see above)
+			drawRecursivePortals(destView, portal->clippedProjMat(destView, projMat), recursionLevel + 1);
+		}
+
+		// Disable color and depth drawing
+		glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+		glDepthMask(GL_FALSE);
+
+		// Enable stencil test and stencil drawing
+		glEnable(GL_STENCIL_TEST);
+		glStencilMask(0xFF);
+
+		// Fail stencil test when inside of our newly rendered
+		// inner portal
+		glStencilFunc(GL_NOTEQUAL, recursionLevel + 1, 0xFF);
+
+		// Decrement stencil value on stencil fail
+		// This resets the incremented values to what they were before,
+		// eventually ending up with a stencil buffer full of zero's again
+		// after the last (outer) step.
+		glStencilOp(GL_DECR, GL_KEEP, GL_KEEP);
+
+		// Draw portal into stencil buffer
+		portal->draw(viewMat, projMat);
 	}
 
-	// Disable stencil test and reset OP
+	// Disable the stencil test and stencil writing
 	glDisable(GL_STENCIL_TEST);
-	glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+	glStencilMask(0x00);
 
-	
+	// Disable color writing
+	glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+
+	// Enable the depth test, and depth writing.
+	glEnable(GL_DEPTH_TEST);
+	glDepthMask(GL_TRUE);
+
+	// Make sure we always write the data into the buffer
+	glDepthFunc(GL_ALWAYS);
+
+	// Clear the depth buffer
+	glClear(GL_DEPTH_BUFFER_BIT);
 
 	// Draw portals into depth buffer
-	glClear(GL_DEPTH_BUFFER_BIT);
-	glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
-	glDepthMask(GL_TRUE);
 	for (Portal *portal : d_portals)
-		portal->draw(player.viewMat(), player.projMat());
+		portal->draw(viewMat, projMat);
 
+	// Reset the depth function to the default
+	glDepthFunc(GL_LESS);
 
-	// Draw scene objects normally
+	// Enable stencil test and disable writing to stencil buffer
+	glEnable(GL_STENCIL_TEST);
+	glStencilMask(0x00);
+
+	// Draw at stencil >= recursionlevel
+	// which is at the current level or higher (more to the inside)
+	// This basically prevents drawing on the outside of this level.
+	glStencilFunc(GL_LEQUAL, recursionLevel, 0xFF);
+
+	// Enable color and depth drawing again
 	glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
 	glDepthMask(GL_TRUE);
-	drawNonPortals(player.viewMat(), player.projMat());
-	
 
+	// And enable the depth test
+	glEnable(GL_DEPTH_TEST);
+
+	// Draw scene objects normally, only at recursionLevel
+	drawNonPortals(viewMat, projMat);
+
+}
+
+void Scene::draw(Player const &player)
+{
+	// Clear all buffers
+	glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+	glDepthMask(GL_TRUE);
+	glStencilMask(0xFF);
+	glClear(GL_STENCIL_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	// Start the recursion
+	drawRecursivePortals(player.viewMat(), player.projMat(), 0);
 }
