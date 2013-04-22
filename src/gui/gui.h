f559a231c5247403bbf9f217e995f742f7656bbc
Thomas Rinsma <thomasrinsma@gmail.com>
2013-04-22 17:05:11 +0200

    replaced some ugly pointers with unique_ptr and references

--- src/gui/gui.h
+++ src/gui/gui.h
@@ -3,13 +3,14 @@
 
 #include <unordered_map>
 #include <string>
+#include <memory>
 #include <SFML/Window.hpp> // for sf::Window
 #include <glm/glm.hpp> // for glm::mat4
 #include "../drawableentity/drawableentity.h"
 
 class GUI
 {
-	std::unordered_map<std::string, DrawableEntity *> d_screenElements;
+	std::unordered_map<std::string, std::unique_ptr<DrawableEntity>> d_screenElements;
 	glm::mat4 d_viewMat;
 	glm::mat4 d_projMat;
 
@@ -17,13 +18,22 @@ class GUI
         GUI(sf::Window &win);
         ~GUI();
 
-        void add(std::string const &name, DrawableEntity *object);
+        void add(std::string const &name, DrawableEntity* object);
         void update(float deltaTime);
         void draw();
 
-        DrawableEntity *get(std::string const &name);
+        DrawableEntity &get(std::string const &name);
+
+        template <typename T>
+        T &get(std::string const &name);
 
     private:
 };
-        
+
+// templated version
+template <typename T>
+T &GUI::get(std::string const &name)
+{
+    return dynamic_cast<T &>(*d_screenElements.at(name));
+}  
 #endif
